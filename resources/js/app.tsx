5f47f6e5c7e7a4d3fdacb2a8a22bbcbf4ca9f252
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2025-05-19 16:46:49 +0200

    fix: layout

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,6 +4,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
+import NavBar from '@/components/nav-bar';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,7 +14,12 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <>
+                <NavBar/>
+                <App {...props} />
+            </>,
+        );
     },
     progress: {
         color: '#4B5563',
