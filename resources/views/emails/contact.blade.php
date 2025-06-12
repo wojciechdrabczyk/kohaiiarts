d7f75c8cec0a6b78d3b04339862ab7fa1371e102
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2025-06-12 15:03:45 +0200

    wip: mailing service

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -6,4 +6,8 @@
 @endif
 
 <p><strong>Message:</strong></p>
-<p>{!! nl2br(e($data['message'])) !!}</p>
+<p>{{ nl2br(e($data['message'])) }}</p>
+
+@foreach ($data['files'] as $url)
+    <img style="max-width: 500px" src="{{ $url }}"/>
+@endforeach
