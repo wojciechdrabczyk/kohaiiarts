52ade3774eaa897559d622317906cc7d2fe26699
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2025-07-01 16:14:43 +0200

    test

--- resources/js/pages/illustrations.tsx
+++ resources/js/pages/illustrations.tsx
@@ -76,7 +76,7 @@ export default function Illustrations() {
                 <meta name="description" content="Here are my latest drawings" />
             </Head>
 
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 pb-8">
+            <div className="grid grid-cols-4 md:grid-cols-4 gap-4 pb-8">
                 {[0, 1, 2, 3].map((colIndex) => (
                     <div key={colIndex} className="grid gap-4">
                         {images
