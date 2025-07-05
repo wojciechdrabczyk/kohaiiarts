d811796564af612b3f84a2adac7d39b048f51019
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2025-07-05 21:21:30 +0200

    style: various changes to components

--- resources/js/components/nav-bar.tsx
+++ resources/js/components/nav-bar.tsx
@@ -1,8 +1,7 @@
 import NewgroundsIcon from '@/assets/icons/NewgroundsIcon';
 import DefaultLayout from '@/layouts/default-layout';
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
-import { Link, usePage } from '@inertiajs/react';
-import { Head } from '@inertiajs/react';
+import { Head, Link, usePage } from '@inertiajs/react';
 import { useLayoutEffect, useState } from 'react';
 import { BsInstagram } from 'react-icons/bs';
 import { FaDiscord } from 'react-icons/fa';
@@ -30,16 +29,16 @@ export default function NavBar() {
 
     const popoverPanelStyles = 'absolute top-full mt-2 rounded-xl w-44 border border-gray-200 bg-white dark:bg-neutral-900 shadow-lg';
     const popoverLinkStyles =
-        'flex items-center gap-2 rounded-xl w-full px-4 py-2 text-sm text-left text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700';
+        'flex items-center gap-2 rounded-xl w-full px-4 py-2 text-sm text-left text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-rose-900';
     const mobileLinkStyles = 'text-gray-500 hover:text-gray-700 dark:text-gray-300';
 
     return (
-        <div className="sticky top-0 z-10">
+        <div className="top-0 z-10">
             <Head>
                 <link rel="preload" href="/img-static/LogoKohii.webp" as="image" />
                 <link rel="preload" href="/img-static/LogoKohiiNight.webp" as="image" />
             </Head>
-            <nav className="bg-white shadow-md dark:bg-black" style={{ fontFamily: 'Montserrat, sans-serif' }}>
+            <nav className="bg-white dark:bg-black" style={{ fontFamily: 'Montserrat, sans-serif' }}>
                 <div className="mx-auto max-w-7xl px-4">
                     <div className="flex h-16 items-center justify-between">
                         <Link href={route('home')} className="flex items-center gap-4 overflow-hidden" prefetch>
@@ -56,7 +55,6 @@ export default function NavBar() {
                                 />
                             </div>
 
-
                             <div className="flex min-w-0 flex-col overflow-hidden leading-tight">
                                 <span className="truncate text-2xl font-light text-gray-800 dark:text-white">Kohaii Arts</span>
                                 <span className="truncate text-xs font-light tracking-wide text-gray-500 dark:text-gray-400">
@@ -68,30 +66,42 @@ export default function NavBar() {
                         <div className="hidden items-center space-x-5 text-lg md:flex">
                             <Link
                                 href={route('home')}
-                                className={`text-gray-500 hover:text-gray-700 dark:text-gray-300 ${url === '/' ? 'text-black underline dark:text-white' : ''}`}
+                                className={`text-gray-500 hover:text-rose-900 dark:text-gray-300 dark:hover:text-rose-800 ${
+                                    url === '/' ? 'text-rose-700 underline underline-offset-4 dark:text-rose-900' : ''
+                                }`}
                                 prefetch
                             >
                                 Illustrations
                             </Link>
-                            <Link href={route('store')} className="text-gray-500 hover:text-gray-700 dark:text-gray-300" prefetch>
+                            <Link
+                                href={route('store')}
+                                className={`text-gray-500 hover:text-rose-900 dark:text-gray-300 dark:hover:text-rose-800 ${
+                                    url === '/store' ? 'text-rose-700 underline underline-offset-4 dark:text-rose-900' : ''
+                                }`}
+                                prefetch
+                            >
                                 Store
                             </Link>
                             <Link
                                 href={route('support')}
-                                className={`text-gray-500 hover:text-gray-700 dark:text-gray-300 ${url === '/support' ? 'text-black underline dark:text-white' : ''}`}
+                                className={`text-gray-500 hover:text-rose-900 dark:text-gray-300 dark:hover:text-rose-900 ${
+                                    url === '/support' ? 'text-rose-700 underline underline-offset-4 dark:text-rose-800' : ''
+                                }`}
                                 prefetch
                             >
                                 Support
                             </Link>
                             <Link
                                 href={route('commissions')}
-                                className={`text-gray-500 hover:text-gray-700 dark:text-gray-300 ${url === '/commissions' ? 'text-black underline dark:text-white' : ''}`}
+                                className={`text-gray-500 hover:text-rose-900 dark:text-gray-300 dark:hover:text-rose-900 ${
+                                    url === '/commissions' ? 'text-rose-700 underline underline-offset-4 dark:text-rose-800' : ''
+                                }`}
                                 prefetch
                             >
                                 Commissions
-                            </Link>
+                            </Link>{' '}
                             <Popover className="relative">
-                                <PopoverButton className="text-gray-500 hover:text-gray-700 focus:ring-0 focus:outline-none dark:text-gray-300">
+                                <PopoverButton className="text-gray-500 hover:text-rose-900 dark:hover:text-rose-900 focus:ring-0 focus:outline-none dark:text-gray-300">
                                     Socials
                                 </PopoverButton>
                                 <PopoverPanel className={popoverPanelStyles}>
@@ -144,14 +154,18 @@ export default function NavBar() {
                             </Popover>
                             <Link
                                 href={route('faq')}
-                                className={`text-gray-500 hover:text-gray-700 dark:text-gray-300 ${url === '/faq' ? 'text-black underline dark:text-white' : ''}`}
+                                className={`text-gray-500 dark:hover:text-rose-800 hover:text-rose-900 dark:text-gray-300 ${
+                                    url === '/faq' ? 'text-rose-700 underline underline-offset-4 dark:text-rose-900 ' : ''
+                                }`}
                                 prefetch
                             >
                                 FAQ
                             </Link>
                             <Link
                                 href={route('contact')}
-                                className={`text-gray-500 hover:text-gray-700 dark:text-gray-300 ${url === '/contact' ? 'text-black underline dark:text-white' : ''}`}
+                                className={`text-gray-500 dark:hover:text-rose-800 hover:text-rose-900 dark:text-gray-300 ${
+                                    url === '/contact' ? 'text-rose-700 underline underline-offset-4 dark:text-rose-900 ' : ''
+                                }`}
                                 prefetch
                             >
                                 Contact
@@ -185,8 +199,9 @@ export default function NavBar() {
                 </div>
 
                 <div
-                    className={`overflow-hidden transition-all duration-200 ease-out md:hidden ${isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'
-                        }`}
+                    className={`overflow-hidden transition-all duration-200 ease-out md:hidden ${
+                        isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'
+                    }`}
                 >
                     <div className="flex flex-col space-y-2 border-t bg-white px-4 pb-4 shadow-md dark:border-neutral-800 dark:bg-black">
                         <Link href={route('home')} className={mobileLinkStyles} onClick={closeMenu} prefetch>
--- resources/js/layouts/default-layout.tsx
+++ resources/js/layouts/default-layout.tsx
@@ -6,7 +6,7 @@ export default function DefaultLayout({ children }: { children: React.ReactNode 
     return (
         <div className="flex flex-col min-h-screen">
             <NavBar />
-            <main className="flex-1 w-full mx-auto max-w-8xl px-4 sm:px-6 lg:px-8 mt-8 dark:text-white">
+            <main className="flex-1 w-full mx-auto max-w-full px-4 sm:px-1 lg:px-1 mt-8 dark:text-white">
                 {children}
             </main>
             <Footer />
